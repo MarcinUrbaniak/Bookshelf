caca125bb764b7c2e9869f96688cff44536f5717
marcinurbaniak <marcin.urbaniak@gmail.com>
2019-08-04 15:41:46 +0200

    create BookshelfApp class

---
+++ src/main/java/com/example/bookshelf/BookshelfApp.java
@@ -0,0 +1,25 @@
+package com.example.bookshelf;
+
+import fi.iki.elonen.NanoHTTPD;
+
+import java.io.IOException;
+
+public class BookshelfApp extends NanoHTTPD {
+
+    public BookshelfApp(int port) throws IOException {
+        super(port);
+        start(5000, false);
+        System.out.println("Server has been started");
+    }
+
+    public static void main(String[] args) {
+        try {
+            new BookshelfApp(8080);
+
+        }catch (IOException e){
+            System.err.println("Server can't startet because of error: \n" + e);
+        }
+    }
+}
+
+
