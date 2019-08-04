5436bb8408e38f916a478bda4a538078715144aa
marcinurbaniak <marcin.urbaniak@gmail.com>
2019-08-04 15:50:23 +0200

    modify Book class with lombok annotations

--- src/main/java/com/example/bookshelf/type/Book.java
+++ src/main/java/com/example/bookshelf/type/Book.java
@@ -1,56 +1,15 @@
 package com.example.bookshelf.type;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Book {
     private long id;
     private String title;
     private String author, publishingHouse;
     private Integer pagesSum, yearOfPublished;
 
-    public long getId() {
-        return id;
-    }
 
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getPublishingHouse() {
-        return publishingHouse;
-    }
-
-    public void setPublishingHouse(String publishingHouse) {
-        this.publishingHouse = publishingHouse;
-    }
-
-    public Integer getPagesSum() {
-        return pagesSum;
-    }
-
-    public void setPagesSum(Integer pagesSum) {
-        this.pagesSum = pagesSum;
-    }
-
-    public Integer getYearOfPublished() {
-        return yearOfPublished;
-    }
-
-    public void setYearOfPublished(Integer yearOfPublished) {
-        this.yearOfPublished = yearOfPublished;
-    }
 }
