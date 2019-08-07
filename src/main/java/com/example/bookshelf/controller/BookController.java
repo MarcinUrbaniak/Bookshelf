ae33a70e5aadbe114b6a17c9d31632dab33e1d31
marcinurbaniak <marcin.urbaniak@gmail.com>
2019-08-07 13:40:23 +0200

    modify serveGetBookRequest in BookController

--- src/main/java/com/example/bookshelf/controller/BookController.java
+++ src/main/java/com/example/bookshelf/controller/BookController.java
@@ -9,7 +9,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import fi.iki.elonen.NanoHTTPD.IHTTPSession;
 import fi.iki.elonen.NanoHTTPD.Response;
 
-import java.io.IOException;
+
 import java.util.List;
 import java.util.Map;
 
