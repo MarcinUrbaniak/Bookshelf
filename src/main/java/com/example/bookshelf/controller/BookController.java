f62160b08e0c4ece1cf8b481177d615eca2a56e8
marcinurbaniak <marcin.urbaniak@gmail.com>
2019-08-07 11:24:37 +0200

    modify serveAddBookRequest in BookController

--- src/main/java/com/example/bookshelf/controller/BookController.java
+++ src/main/java/com/example/bookshelf/controller/BookController.java
@@ -3,27 +3,28 @@ package com.example.bookshelf.controller;
 import com.example.bookshelf.storage.BookStorage;
 import com.example.bookshelf.storage.impl.StaticListBookStorageImpl;
 
+import com.example.bookshelf.type.Book;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import fi.iki.elonen.NanoHTTPD.IHTTPSession;
 import fi.iki.elonen.NanoHTTPD.Response;
+
+import java.io.IOException;
 
 import static fi.iki.elonen.NanoHTTPD.Response.Status.INTERNAL_ERROR;
 import static fi.iki.elonen.NanoHTTPD.Response.Status.OK;
 import static fi.iki.elonen.NanoHTTPD.newFixedLengthResponse;
 
 
-
-
 public class BookController {
 
     private BookStorage bookStorage = new StaticListBookStorageImpl();
 
-    public Response serveGetBookRequest(IHTTPSession session){
+    public Response serveGetBookRequest(IHTTPSession session) {
         return null;
     }
 
-    public Response serveGetBooksRequest(IHTTPSession session){
+    public Response serveGetBooksRequest(IHTTPSession session) {
 
         ObjectMapper objectMapper = new ObjectMapper();
         String response = "";
@@ -31,16 +32,36 @@ public class BookController {
         try {
             response = objectMapper.writeValueAsString(bookStorage.getAllBooks());
 
-        }catch (JsonProcessingException e){
-            System.err.println("Error during process request: \n"+ e);
+        } catch (JsonProcessingException e) {
+            System.err.println("Error during process request: \n" + e);
             return newFixedLengthResponse(INTERNAL_ERROR, "text/plain", "Internal error, can't read all books");
         }
 
         return newFixedLengthResponse(OK, "application/json", response);
     }
 
-    public Response serveAddBookRequest(IHTTPSession session){
-        return null;
+    public Response serveAddBookRequest(IHTTPSession session) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        long randomBookId = System.currentTimeMillis();
+
+        String lengthHeader = session.getHeaders().get("content-length");
+        int contentLength = Integer.parseInt(lengthHeader);
+        byte[] buffer = new byte[contentLength];
+
+        try {
+            session.getInputStream().read(buffer, 0, contentLength);
+            String requestBody = new String(buffer).trim();
+            Book requestBook = objectMapper.readValue(requestBody, Book.class);
+            requestBook.setId(randomBookId);
+
+            bookStorage.addBook(requestBook);
+
+        } catch (Exception e) {
+            System.err.println("Error during process request: \n" + e);
+            return newFixedLengthResponse(INTERNAL_ERROR, "text/plain", "Internal error book hasn't been added");
+        }
+
+        return newFixedLengthResponse(OK, "text/plain", "Book has been successfully added. id = " + randomBookId);
     }
 
 }
